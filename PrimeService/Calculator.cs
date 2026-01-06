086c180b59938766d5a5cbc41e661aae137e91fc
ravikumar-cybage <131234846+ravikumar-cybage@users.noreply.github.com>
2026-01-06 21:54:28 +0530

    Update Calculator.cs

--- PrimeService/Calculator.cs
+++ PrimeService/Calculator.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-namespace Prime.Services{
-    public class Calculator{
-        public int Add(int a, int b){
-            return a + b;}
-public int Subtract(int a, int b){
-            return a - b;}
- public int Multiply(int a, int b){
-            return a * b;}}}
+namespace Prime.Services
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+public int Subtract(int a, int b)
+{
+            return a - b;
+}
+ public int Multiply(int a, int b)
+ {
+            return a * b;
+ }
+    }
+}
+
 
