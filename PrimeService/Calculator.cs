4bad27fb95334cdc094ac377497dc13f34e156e4
ravikumar-cybage <131234846+ravikumar-cybage@users.noreply.github.com>
2026-01-06 19:39:13 +0530

    Add files via upload

---
+++ PrimeService/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
