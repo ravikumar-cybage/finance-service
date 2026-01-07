bcf5e06ca11de80c617cb081b42adeb77ec9e808
ravikumar-cybage <131234846+ravikumar-cybage@users.noreply.github.com>
2026-01-08 00:27:29 +0530

    Update Calculator.cs

--- PrimeService/Calculator.cs
+++ PrimeService/Calculator.cs
@@ -15,7 +15,14 @@ public int Subtract(int a, int b)
  {
             return a * b;
  }
-    }
+ public int Divide(int a, int b)
+ {
+   if (b == 0)
+       throw new ArgumentException("Cannot divide by zero");
+   return a / b;
+ }
 }
+}
+
 
 
